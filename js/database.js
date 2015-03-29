02ec0373424b3e5fae6e2cd3bec7f192d00a3a8c
Joe Duncko <dotmaners@gmail.com>
2015-03-28 21:32:02 -0400

    Addition to last commit

---
+++ js/database.js
@@ -0,0 +1,78 @@
+// Helper function that takes a new score snapshot and adds an appropriate row to our leaderboard table.
+function handleScoreAdded(scoreSnapshot, prevScoreName) {
+	console.log("handleScoreAdded");
+
+
+	if (scoreSnapshot.val().team === "blue") {
+		console.log("add " + scoreSnapshot.val().animal + scoreSnapshot.val().number + " to blue team");
+
+		var newScoreRow = $("<tr/>");
+		newScoreRow.append($("<td/>").text(scoreSnapshot.val().voteCount));
+		newScoreRow.append($("<td/>").text(scoreSnapshot.val().animal + scoreSnapshot.val().number));
+		newScoreRow.append($("<td/>").text(scoreSnapshot.val().pointCount));
+
+		// Store a reference to the table row so we can get it again later.
+		htmlForPath[scoreSnapshot.key()] = newScoreRow;
+
+		// Insert the new score in the appropriate place in the table.
+
+		$("#blueTeamTable").append(newScoreRow);
+
+		/*
+		if (prevScoreName === null) {
+			$("#blueTeamTable").append(newScoreRow);
+		}
+		else {
+			var lowerScoreRow = htmlForPath[prevScoreName];
+			lowerScoreRow.before(newScoreRow);
+		}
+		*/
+	}
+	if (scoreSnapshot.val().team === "red") {
+		console.log("add " + scoreSnapshot.val().animal + scoreSnapshot.val().number + " to red team");
+
+		var newScoreRow = $("<tr/>");
+		newScoreRow.append($("<td/>").text(scoreSnapshot.val().voteCount));
+		newScoreRow.append($("<td/>").text(scoreSnapshot.val().animal + scoreSnapshot.val().number));
+		newScoreRow.append($("<td/>").text(scoreSnapshot.val().pointCount));
+
+		// Store a reference to the table row so we can get it again later.
+		htmlForPath[scoreSnapshot.key()] = newScoreRow;
+
+		// Insert the new score in the appropriate place in the table.
+		$("#redTeamTable").append(newScoreRow);
+
+		/*
+		if (prevScoreName === null) {
+			$("#redTeamTable").append(newScoreRow);
+		}
+		else {
+			var lowerScoreRow = htmlForPath[prevScoreName];
+			lowerScoreRow.before(newScoreRow);
+		}
+		*/
+	}
+
+	if (scoreSnapshot.val().animal == useranimal && scoreSnapshot.val().number == usernumber) {
+		//Pull data for...
+		console.log("Pull my user data");
+		cachedCanToss = scoreSnapshot.val().canToss;
+		cachedPointCount = scoreSnapshot.val().pointCount;
+		cachedVoteCount = scoreSnapshot.val().voteCount;
+		cachedVotesAvaliable = scoreSnapshot.val().votesAvaliable;
+	}
+
+/*
+	// Store a reference to the table row so we can get it again later.
+	htmlForPath[scoreSnapshot.key()] = newScoreRow;
+
+	// Insert the new score in the appropriate place in the table.
+	if (prevScoreName === null) {
+		$("#leaderboardTable").append(newScoreRow);
+	}
+	else {
+		var lowerScoreRow = htmlForPath[prevScoreName];
+		lowerScoreRow.before(newScoreRow);
+	}
+*/
+}
